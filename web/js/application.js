ca19f2cc0269a7fe4156fa604feffee216e47d57
scalpa01 <franche.pascal01@gmail.com>
2011-07-26 15:07:10 +0000

    new javascript project

---
+++ web/js/application.js
@@ -0,0 +1,8 @@
+$(function () {
+     $('.titre').text(document.title);  
+     
+     var url = 'http://chart.apis.google.com/chart?cht=p3&chs=290x130&chl=a,b,c&chd=t:1,2,3&chds=0,3&chf=bg,s,d0e4fe';
+     
+    $('#chart img').attr('src', url);
+
+});
