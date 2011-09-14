45711444aa99b14207782240cb74ccd3f9d66773
Pascal Franche <franche.pascal01@gmail.com>
2011-09-13 20:39:36 -0400

    second commit

--- web/js/application.js
+++ web/js/application.js
@@ -1,4 +1,4 @@
-(function () {
+$(function () {
      $('.titre').text(document.title);  
      
      var url = 'http://chart.apis.google.com/chart?cht=p3&chs=290x130&chl=a,b,c&chd=t:1,2,3&chds=0,3&chf=bg,s,d0e4fe';
